f33632b63b3e262d884dc712a7f6d739dc63f356
kostyaFrom <korustlt@gmail.com>
2022-01-14 12:41:02 +0300

    implement view type and pool for rv

--- app/src/main/java/com/konstantinbulygin/listtoshop/presentation/MainActivity.kt
+++ app/src/main/java/com/konstantinbulygin/listtoshop/presentation/MainActivity.kt
@@ -7,41 +7,33 @@ import android.view.LayoutInflater
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.konstantinbulygin.listtoshop.R
 import com.konstantinbulygin.listtoshop.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
-    private lateinit var llShopList: LinearLayout
+    private lateinit var adapter: ShopItemListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        llShopList = findViewById(R.id.ll_shop_list)
+        setupRecyclerView()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
-            showList(it)
+            adapter.shopItemList = it
+
         }
     }
-    private fun showList(list: List<ShopItem>) {
-        llShopList.removeAllViews()
-        for(shopItem in list) {
-            val layoutId = if(shopItem.isEnable) {
-                R.layout.item_shop_enabled
-            } else {
-                R.layout.item_shop_disabled
-            }
-            val view = LayoutInflater.from(this).inflate(layoutId, llShopList, false)
-            val tvName = view.findViewById<TextView>(R.id.tv_name)
-            val tvCount = view.findViewById<TextView>(R.id.tv_count)
-            tvName.text = shopItem.name
-            tvCount.text = shopItem.count.toString()
-            view.setOnLongClickListener {
-                viewModel.changeEnableState(shopItem)
-                true
-            }
-            llShopList.addView(view)
+
+    private fun setupRecyclerView() {
+        val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
+        adapter = ShopItemListAdapter()
+        with(rvShopList) {
+            adapter = adapter
+            recycledViewPool.setMaxRecycledViews(ShopItemListAdapter.VIEW_TYPE_ENABLED, ShopItemListAdapter.VIEW_HOLDER_MAX_POOL_SIZE)
+            recycledViewPool.setMaxRecycledViews(ShopItemListAdapter.VIEW_TYPE_DISABLED, ShopItemListAdapter.VIEW_HOLDER_MAX_POOL_SIZE)
         }
     }
 }
