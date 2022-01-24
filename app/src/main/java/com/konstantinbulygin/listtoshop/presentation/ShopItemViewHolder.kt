511d8e31284b672b213d6bd29031974ce8d897d5
kostyaFrom <korustlt@gmail.com>
2022-01-24 14:58:04 +0300

    implement data binding to adapter

--- app/src/main/java/com/konstantinbulygin/listtoshop/presentation/ShopItemViewHolder.kt
+++ app/src/main/java/com/konstantinbulygin/listtoshop/presentation/ShopItemViewHolder.kt
@@ -1,11 +1,7 @@
 package com.konstantinbulygin.listtoshop.presentation
 
-import android.view.View
-import android.widget.TextView
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
-import com.konstantinbulygin.listtoshop.R
 
-class ShopItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    val tvName: TextView = view.findViewById(R.id.tv_name)
-    val tvCount: TextView = view.findViewById(R.id.tv_count)
-}
+class ShopItemViewHolder(val binding: ViewDataBinding) :
+    RecyclerView.ViewHolder(binding.root)
